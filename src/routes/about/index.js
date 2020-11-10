e2662c57a9b4e81e1d0917d35bbd5d9769e168c2
Adobe-Android <tngamerboy@gmail.com>
2020-11-09 22:08:48 -0600

    Added backgrounds to remaining pages

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -4,12 +4,14 @@ import style from './style.css';
 
 // Note: `user` comes from the URL, courtesy of our router
 const Profile = () => (
-	<div class={style.container}>
-		<h1 class={style.profile}>About Me</h1>
-		<section class={style.sectionBorder}></section>
-		<p class={style.blockText}>
-			I'm a software developer at Zycron with Nashville Electric Service. I'm passionate about software craftsmanship, open source software, and helping others solve problems with technical solutions. I always welcome a good challenge and an opportunity to learn something new. I look forward to the many years ahead of me in this industry.
-		</p>
+	<div class={style.about}>
+		<div class={style.container}>
+			<h1 class={style.profile}>About Me</h1>
+			<section class={style.sectionBorder}></section>
+			<p class={style.blockText}>
+				I'm a software developer at Zycron with Nashville Electric Service. I'm passionate about software craftsmanship, open source software, and helping others solve problems with technical solutions. I always welcome a good challenge and an opportunity to learn something new. I look forward to the many years ahead of me in this industry.
+			</p>
+		</div>
 	</div>
 );
 
